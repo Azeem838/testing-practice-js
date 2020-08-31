b14a1916c9975487f230a0b07e8d4d8e642a4bde
Azeem <azeemahmed101@gmail.com>
2020-08-31 17:28:07 +0200

    refactor calculator to another file

--- strings.js
+++ strings.js
@@ -1,27 +1,9 @@
 const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
 
 const reverseString = (string) => {
-  return string
-    .split('')
-    .reverse()
-    .join('')
-}
+  return string.split('').reverse().join('');
+};
 
-const calculator = {
-  add: (a, b) => {
-    return a + b
-  },
-  subtract: (a, b) => {
-    return a - b
-  },
-  divide: (a, b) => {
-    return a / b
-  },
-  multiply: (a, b) => {
-    return a * b
-  },
-}
-
-export { calculator, reverseString, capitalize }
+export { reverseString, capitalize };
--- strings.test.js
+++ strings.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './strings'
+import { capitalize, reverseString } from './strings';
 
 test('takes a string and returns that string with the first character capitalized.', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -6,11 +6,4 @@ test('takes a string and returns that string with the first character capitalize
 
 test('takes a string and returns it reversed', () => {
   expect(reverseString('allo')).toBe('olla');
-});
-
-test('checks that the calculator object contains the basic operations', () => {
-  expect(calculator.add(2, 8)).toBe(10);
-  expect(calculator.subtract(2, 8)).toBe(-6);
-  expect(calculator.divide(2, 8)).toBe(0.25);
-  expect(calculator.multiply(2, 8)).toBe(16);
 });
